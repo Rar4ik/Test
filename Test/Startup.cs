1f7301bc9b2720cc861b41854c0cef12ff930fc0
Rar4ik <mravaev@yandex.ru>
2020-01-15 11:59:26 +0300

    start string

--- Test/Startup.cs
+++ Test/Startup.cs
@@ -16,6 +16,7 @@ namespace Test
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var start_string = "this is the start string for te git";
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
